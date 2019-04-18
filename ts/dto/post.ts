d919a40618023f1570a9561eadbde8cb98e08d13
Patrick Toner <pat.toner2@gmail.com>
2019-04-17 20:04:57 -0400

    Post DTO

---
+++ ts/dto/post.ts
@@ -0,0 +1,21 @@
+class Post {
+
+    public id: number
+    public owner: string 
+    public ipfsCid: string 
+    public title: string 
+    public subtitle: string 
+    public coverPhoto: string 
+    public dateCreated: string 
+    public authorId: number
+    // public author:Profile  
+    public content: any 
+    // public contentTranslated: string 
+
+}
+
+export {
+    Post
+}
+
+
